6f1c165de0982df986ef4779aa1197ca83292166
Rey Mercado <reymercado.usa@gmail.com>
2024-02-16 18:24:04 -0800

    chore: created a new Instruction destination (step 9) also fixed several mistakes in prior steps 6 through 8 (layouts and navigation)

--- starter/app/src/main/java/com/udacity/shoestore/LoginFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/LoginFragment.kt
@@ -6,18 +6,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.udacity.shoestore.databinding.FragmentLoginBinding
 
 /**
  * A simple [Fragment] subclass.
  */
 class LoginFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?): View {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
         val binding: FragmentLoginBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_login, container, false)
+
+        binding.loginButton.setOnClickListener { v: View ->
+            v.findNavController()
+                .navigate(LoginFragmentDirections.actionLoginFragmentToWelcomeFragment())
+        }
         return binding.root
+//        binding.loginButton.setOnClickListener(v ->
+//            Navigation.findNavController(R.id.action_loginFragment_to_welcomeFragment)
+//        )
     }
 
 }
