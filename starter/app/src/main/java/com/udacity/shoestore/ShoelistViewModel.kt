c70c1f7fcf35ceadd0a4e2e3b7b741767205bd21
Rey Mercado <reymercado.usa@gmail.com>
2024-02-22 17:03:18 -0800

    chore: used an Activity level `ViewModel` to hold a list of Shoes (using by `activityViewModels`) (step 15, first bullet)

--- starter/app/src/main/java/com/udacity/shoestore/ShoelistViewModel.kt
+++ starter/app/src/main/java/com/udacity/shoestore/ShoelistViewModel.kt
@@ -1,13 +1,20 @@
 package com.udacity.shoestore
 
 import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.udacity.shoestore.models.Shoe
 
 class ShoelistViewModel : ViewModel() {
 
-    private lateinit var shoeList: MutableLiveData<MutableList<Shoe>>
+    private val _shoeList = MutableLiveData<MutableList<Shoe>>(mutableListOf())
+    val shoeList: LiveData<MutableList<Shoe>>
+        get() = _shoeList
+
+    fun saveShoeList(newShoeList: MutableList<Shoe>) {
+        _shoeList.value = newShoeList
+    }
 
     init {
         Log.i("ShoelistViewModel", "ShoelistViewModel created!")
